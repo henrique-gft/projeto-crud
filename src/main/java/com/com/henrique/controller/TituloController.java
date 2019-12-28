43d08b3ef1cdeecd073227db1b53f6d2d6e9640d
Henrique <henriquevergara@hotmail.com>
2019-12-28 17:25:42 -0300

    Conexao com banco de dados
    Inclusao de titulo no banco de dados MYSQL

--- src/main/java/com/com/henrique/controller/TituloController.java
+++ src/main/java/com/com/henrique/controller/TituloController.java
@@ -1,14 +1,43 @@
 package com.com.henrique.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.com.henrique.model.StatusTitulo;
+import com.com.henrique.model.Titulo;
+import com.com.henrique.repository.Titulos;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-	@RequestMapping("/titulos/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	@Autowired
+	private Titulos titulos;
+	
+	@ModelAttribute(value = "statusEnum")
+	public List<StatusTitulo> statusEnum(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+	
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Título salvo com sucesso!");
+		return mv;
 	}
 	
 }
---
+++ src/main/java/com/com/henrique/repository/Titulos.java
@@ -0,0 +1,11 @@
+package com.com.henrique.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.com.henrique.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long>{
+
+	
+	
+}
